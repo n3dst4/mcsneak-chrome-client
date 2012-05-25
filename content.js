e64766a5e93570d933a17586172e01aa2ef518b4
Neil de Carteret <n3dst4@gmail.com>
2012-05-25 11:31:06 +0100

    initial import

---
+++ content.js
@@ -0,0 +1,8 @@
+
+//chrome.extension.onRequest.addListener(
+//    function(request, sender, sendResponse) {
+//        if (request.reload) {
+//            window.location.reload();
+//        }
+//    }
+//);
